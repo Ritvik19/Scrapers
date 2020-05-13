55b4b2d73abe0fcd92e2d17277dfb3987629e203
Ritvik19 <rastogiritvik99@gmail.com>
2020-05-13 10:01:33 +0530

    Project Euler

--- ProjectEuler/ProjectEuler/spiders/problems.py
+++ ProjectEuler/ProjectEuler/spiders/problems.py
@@ -4,12 +4,12 @@ class ProblemsSpider(scrapy.Spider):
     name = 'problems'
 
     def start_requests(self):
-        urls = ['https://projecteuler.net/problem='+str(i) for i in range(1,704)]
+        urls = ['https://projecteuler.net/problem='+str(i) for i in range(1,706)]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         yield {
             'title': response.xpath('//h2[1]/text()').get(),
-            'price': ' '.join(response.xpath('//div[@class="problem_content"]//text()').getall()),
+            'description': ' '.join(response.xpath('//div[@class="problem_content"]//text()').getall()),
         }
